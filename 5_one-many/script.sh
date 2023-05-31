bec7ee30024e4272e67bea04e5a52ea7d063a376
DimaDmitrii <d.deshura@yandex.ru>
2023-06-01 00:09:53 +0300

    send

---
+++ 5_one-many/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+client_amount=$1
+
+for (( counter=0; counter<client_amount; counter++ ))
+do
+        echo `gnome-terminal -- sh -c "bash -c \"./client client$counter; exec bash\""`
+done
+
